b133dafb59b60de3a281a5a70fb0789229b62049
HarleyQuinn22d3 <kdd19981998@gmail.com>
2025-06-14 16:43:58 +0500

    Добавлены недостающие файлы

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using EducationHierarchy;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Test[] tests = new Test[6];
+
+       
+        tests[0] = new Test();
+        tests[0].RandomInit();
+
+        tests[1] = new Exam();
+        tests[1].RandomInit();
+
+        tests[2] = new FinalExam();
+        tests[2].RandomInit();
+
+        tests[3] = new Trial();
+        tests[3].RandomInit();
+
+        tests[4] = new Exam("Ручной ввод", 90, 75, "Смирнов А.А.", true);
+        tests[5] = new FinalExam("Физика", 180, 70, "Петрова М.И.", false, "Государственный аттестат", true);
+
+     
+        Console.WriteLine("=== Просмотр массива объектов ===");
+        foreach (var test in tests)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"Тип объекта: {test.GetType().Name}");
+            test.Show(); 
+            NonVirtualShow(test); 
+        }
+    }
+
+ 
+    static void NonVirtualShow(Test test)
+    {
+        Console.WriteLine("\n[Невиртуальный метод]");
+        Console.WriteLine($"Тест: {test.Name}");
+        Console.WriteLine($"Длительность: {test.Duration} мин");
+        Console.WriteLine($"Проходной балл: {test.PassingScore}%");
+    }
+}
